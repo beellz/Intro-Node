be9295d733908d225ba8bed8f28614d84464ca0b
belal <belal20795@gmail.com>
2020-05-06 13:32:40 +0530

    adding suppervillan challenge

---
+++ index.js
@@ -0,0 +1,10 @@
+const supervillains = require('supervillains');
+ 
+let superVillanAll = supervillains.all;
+//=> ['Abattoir', 'Able Crown', …]
+ 
+let superVillan = supervillains.random();
+//=> 'Mud Pack'
+
+console.log(superVillan);
+// console.log(superVillanAll);
